6e759ef19404ec3b7ccd6efd109be9c5deee2d86
guill <guill@GUILLERMO-PC>
2024-05-17 21:13:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = 5;
+
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero * i;
+            Console.WriteLine($"{numero} x {i} = {resultado}");
+        }
+    }
+}
